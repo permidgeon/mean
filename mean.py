1870e8e6c568449b8baf5872a5f0eb2dc782d822
permidgeon <luchang@berkeley.edu>
2015-06-05 15:17:37 -0700

    New files about mean

---
+++ mean.py
@@ -0,0 +1,14 @@
+def mean(num_list):
+    a = sum(num_list)/len(num_list)
+    if isinstance(a,complex):
+        return NotImplemented
+    try:
+        return sum(num_list)/len(num_list)
+    except ZeroDivisionError as detail :
+        msg = "The algebraic mean of an empty list is undefined."
+        msg += "Please provide a list of numbers."
+        raise ZeroDivisionError(detail.__str__() + "\n" +  msg)
+    except TypeError as detail :
+        msg = "The algebraic mean of an non-numerical list is undefined."
+        msg += "Please provide a list of numbers."
+        raise TypeError(detail.__str__() + "\n" +  msg)
